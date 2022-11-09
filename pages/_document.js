2f8d94f796bc516ff6f50b658a09bbf7b7bd8216
Jake Warren <jwarshack@gmail.com>
2022-11-09 13:11:29 -0800

    meta tag

--- pages/_document.js
+++ pages/_document.js
@@ -2,8 +2,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html>
-      <Head />
+    <Html lang='en'>
+      <Head>
+      <meta
+        name="description"
+        content="Shopify Expert with over 3 years experience using the platform. Based in Newport Beach, CA."
+      />
+      </Head>
       <body className='font-montserrat'>
         <Main />
         <NextScript />
