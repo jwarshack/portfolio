7f7b60051ddc55a8409bb0353ba67ad5278c06bd
Jake Warren <jwarshack@gmail.com>
2022-11-09 12:39:46 -0800

    fixed header

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,7 +5,7 @@ import logo from '../public/img/logo.png'
 
 function Navbar() {
   return (
-    <div className='flex px-10 py-2 w-full text-white bg-transparent fixed justify-between items-center z-10'>
+    <div className='flex px-10 py-2 w-full text-white bg-transparent absolute justify-between items-center z-10'>
       <Link href='/'><Image src={logo} height={60} width={60} alt='JW logo'/></Link>
       <ul className='flex gap-4'>
         <Link href='/portfolio'><li className='hover:text-gray-300'>Portfolio</li></Link>
