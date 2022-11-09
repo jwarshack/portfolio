e1df3cbec52d9e4bd4eb5ddc17ee102e79057d55
Jake Warren <jwarshack@gmail.com>
2022-11-09 13:21:28 -0800

    overflow none

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
 
 
   return (
-   <div className='flex flex-col min-h-screen'>
+   <div className='flex flex-col min-h-screen overflow-hidden'>
     <Head>
       <title>Jake Warren</title>
     </Head>
